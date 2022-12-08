f63fd85c1c7cd9b4b16596a6831ae3d9d69cec8b
Jae Young Park <jae.park@Jaes-MacBook-Pro.local>
2022-12-07 22:40:49 -0800

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://5an7a1rvm5.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
